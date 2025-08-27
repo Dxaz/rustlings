eaca83cd26cc69ce10f2719d44d7f1163a64f892
Dxaz <diaz.gregory@outlook.com>
2025-08-26 23:43:59 -0500

    finished lifetimes

--- exercises/16_lifetimes/lifetimes1.rs
+++ exercises/16_lifetimes/lifetimes1.rs
@@ -4,7 +4,7 @@
 // not own their own data. What if their owner goes out of scope?
 
 // TODO: Fix the compiler error by updating the function signature.
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'input>(x: &'input str, y: &'input str) -> &'input str {
     if x.len() > y.len() {
         x
     } else {
